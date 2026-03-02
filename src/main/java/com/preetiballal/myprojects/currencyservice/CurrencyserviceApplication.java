545e2093a9d3d2d8c76999c70b3c876278115bc6
Preeti Ballal <preetiballal.29@gmail.com>
2026-03-02 13:45:38 +0900

    Test Commit to check connection

---
+++ src/main/java/com/preetiballal/myprojects/currencyservice/CurrencyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.preetiballal.myprojects.currencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyserviceApplication.class, args);
+	}
+
+}
